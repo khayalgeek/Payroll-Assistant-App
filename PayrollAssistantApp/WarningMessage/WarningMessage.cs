139137ff83d97f04123f4a99f35bd4b1602c54b2
khayalnb <xeyal.binnetov@profit.az>
2023-08-24 11:37:41 +0400

    created warning message

--- PayrollAssistantApp/WarningMessage/WarningMessage.cs
+++ PayrollAssistantApp/WarningMessage/WarningMessage.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace PayrollAssistantApp.WarningMessage
 {
-    internal class WarningMessage
+    public class WarningMessage
     {
+        public string emptyValueMessage = "Boş dəyər daxil etmisiz !";
+
+        public string enterMessage = "Daxil et: ";
+
+        public string wrongLetterMessage = "Daxil edilmiş dəyərin terkibində hərf ola bilməz !";
+
+        public string enterSalaryMessage = "Əmək haqqını daxil edin : ";
+
+        public string wrongValueMessage = "Zəhmət olmasa duzgun dəyər daxil edin!";
+
+        public string wrongSalaryMessage = "Zəhmət olmasa əmək haqqını düzgün daxil edin!";
+
+        public string enterMartialStatusMessage = "Zəhmət olmasa ailə vəziyyəti ilə bağlı aşağıdakı seçimlərdən özünüzə uğun halı daxil edin :";
+
+        public string wrongMaritalStatusMessage = "Zəhmət olmasa ailə vəziyyəti məlumatinizi duzgun daxil edin";
+
+        public string enterChildCountMessage = "Zehmet olmasa uşaqların sayın qeyd edin :";
+
+        public string successfulMessage = "Proses uğurla davam edir";
+
+        public string wrongChildCountMessage = "Zəhmət olmasa uşaq sayını düzgün daxil edin !";
+
     }
 }
